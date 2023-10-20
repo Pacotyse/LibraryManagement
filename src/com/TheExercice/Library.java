b2a72b96947cecd5f087b12665018adf36d9bc25
Pacotyse <e.tison85@gmail.com>
2023-10-20 19:54:06 +0200

    ADD: Method to handle books management

--- src/com/TheExercice/Library.java
+++ src/com/TheExercice/Library.java
@@ -1,10 +1,32 @@
 package com.TheExercice;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class Library {
-    private List<Book> books = new ArrayList<>();
-    private List<User> users = new ArrayList<>();
-    private List<Loan> loans = new ArrayList<>();
+    private static List<Book> books = new ArrayList<>();
+    private static List<User> users = new ArrayList<>();
+    private static List<Loan> loans = new ArrayList<>();
+
+    public static void viewBook() {
+        books.stream().forEach(System.out::println);
+    }
+
+    public static void addBook(String title, String author, int releaseDate) {
+        Book book = new Book(title, author, releaseDate);
+        books.add(book);
+    }
+
+    public static void deleteBook(String title) {
+        Iterator<Book> iterator = books.iterator();
+        while (iterator.hasNext()) {
+            Book book = iterator.next();
+            if (book.getTitle().equals(title)) {
+                iterator.remove();
+                System.out.println("Livre supprimé : " + book.toString());
+            }
+        }
+    }
+
 }
--- src/com/TheExercice/Main.java
+++ src/com/TheExercice/Main.java
@@ -2,9 +2,9 @@ package com.TheExercice;
 
 public class Main {
     public static void main(String[] args) {
-        Book book = new Book("2001 l'Odyssey de l'espace", "Arthur Clark", 1981);
-        User user = new User("Enzo");
-        Loan loan = new Loan(book, user);
-        System.out.println(loan.toString());
+        Library.addBook("2001 l'Odyssey de l'espace", "Arthur Clark", 1981);
+        Library.addBook("Jujutsu Kaisen", "Gege Akutani", 2018);
+        Library.deleteBook("Jujutsu Kaisen");
+        Library.viewBook();
     }
 }
