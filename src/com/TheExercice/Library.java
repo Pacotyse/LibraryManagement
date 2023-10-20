1246d7f8345ed13016f9fa69634f11055ceaf501
Pacotyse <e.tison85@gmail.com>
2023-10-20 18:48:14 +0200

    ADD: new class library who can handle books, users and loans

---
+++ src/com/TheExercice/Library.java
@@ -0,0 +1,10 @@
+package com.TheExercice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Library {
+    private List<Book> books = new ArrayList<>();
+    private List<User> users = new ArrayList<>();
+    private List<Loan> loans = new ArrayList<>();
+}
