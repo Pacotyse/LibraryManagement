421d1b258858118a6885ff73df9685a75aa94d1f
Pacotyse <e.tison85@gmail.com>
2023-10-23 00:31:39 +0200

    Adding a new interface manager handling all basics interactions this Book, User and Loan

---
+++ src/com/TheExercice/Manager.java
@@ -0,0 +1,31 @@
+package com.TheExercice;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+public interface Manager {
+    /**
+     * This method can find an item in a specified ArrayList based on a given Predicate.
+     *
+     * @param list      The ArrayList where all data is stored.
+     * @param predicate A Predicate that defines the condition for the item to be found.
+     * @param <T>       The type of elements in the ArrayList and the Predicate.
+     * @return The first item in the ArrayList that satisfies the given Predicate, or null if no such item is found.
+     */
+    public static <T> T findBy(List<T> list, Predicate<T> predicate) {
+        for (T item : list) {
+            if (predicate.test(item)) {
+                return item;
+            }
+        }
+        return null;
+    }
+
+    void viewAll();
+
+    void viewOne();
+
+    void add();
+
+    void delete();
+}
